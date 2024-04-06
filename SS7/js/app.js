d630d4b1f4abe188e44deff92d79814de441c8a8
Dang Duong <dangduong0127@gmail.com>
2024-04-06 19:08:59 +0700

    commit

--- SS7/js/app.js
+++ SS7/js/app.js
@@ -5,32 +5,46 @@ const sendMessForm = document.getElementById("send-messages-form");
 
 let basket = async function () {
   try {
-    const data = await db
+    const chatDocRef = db
       .collection("conversation")
       .doc("con1")
-      .collection("chat")
-      .get();
+      .collection("chat");
+
+    const data = await chatDocRef.get();
     console.log(data);
 
     listMess.innerHTML = null;
+    let messArr = [];
     data.forEach((doc) => {
       let dataFromDB = doc.data().messages;
-      console.log(dataFromDB);
-
       dataFromDB.forEach((item) => {
-        listMess.innerHTML += `
-        <div class="message-container ${
-          auth.currentUser.email === item.email ? `mine` : `their`
-        }">
-            ${
-              auth.currentUser.email !== item.email
-                ? `<div class="owner">${item.email}</div>`
-                : ``
-            }
-            <div class="content">${item.content}</div>
-        </div>
-      `;
+        messArr.push(item);
       });
+    });
+
+    messArr.sort(function (a, b) {
+      return new Date(a.timestamp) - new Date(b.timestamp);
+    });
+    messArr.forEach((item) => {
+      listMess.innerHTML += `
+              <div class="message-container ${
+                auth.currentUser.email === item.email ? `mine` : `their`
+              }">
+                  ${
+                    auth.currentUser.email !== item.email
+                      ? `<div class="owner">${item.email}</div>`
+                      : ``
+                  }
+                  <div class="content">${item.content}
+                    <span style="display: block; font-size: 13px; opacity: 75%">${
+                      new Date(item.timestamp).getHours() +
+                      ":" +
+                      new Date(item.timestamp).getMinutes()
+                    }</span>
+                  </div>
+                  
+              </div>
+            `;
     });
   } catch (err) {
     console.log(err);
@@ -65,15 +79,17 @@ sendMessForm.addEventListener("submit", function (e) {
             // Sử dụng arrayUnion để thêm một phần tử vào mảng
             email: auth.currentUser.email,
             content: message,
+            timestamp: new Date().toString(),
           }),
         },
         { merge: true }
       );
+
+      sendMessForm.reset();
     } catch (err) {
       console.log(err);
     }
   }
-
   sendMess();
   basket();
 });
